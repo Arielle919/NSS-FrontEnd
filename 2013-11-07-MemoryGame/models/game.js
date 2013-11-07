9f70215b0385e2b6cbd10afd8ac892ffa7256b63
Arielle <arielle919@gmail.com>
2013-11-07 14:15:45 -0600

    memory game box show but db not working

---
+++ 2013-11-07-MemoryGame/models/game.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var _ = require('lodash');
+
+var Game = mongoose.Schema({
+  actual       : {type: Number, default: function(){return _.sample([0, 1, 2]);}},
+  guess        : Number,
+  didWin       : {type: Boolean, default: false},
+  player       : String,
+  boxnumber    : Number,
+  createdAt    : {type: Date, default: Date.now}
+});
+
+mongoose.model('Game', Game);
