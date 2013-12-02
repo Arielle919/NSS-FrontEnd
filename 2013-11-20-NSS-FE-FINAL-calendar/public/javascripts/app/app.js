a5d6d6f3ee75970ccd727d18cf0527d7d62ad98a
Arielle <arielle919@gmail.com>
2013-12-02 12:58:24 -0600

    update

--- 2013-11-20-NSS-FE-FINAL-calendar/public/javascripts/app/app.js
+++ 2013-11-20-NSS-FE-FINAL-calendar/public/javascripts/app/app.js
@@ -11,9 +11,7 @@ function initialize(){
   $('#register').on('click', clickRegister);
   $('#login').on('click', clickLogin);
   $('#show-contactForm').on('click', showForm);
-  // $('form#appointment').on('submit', submitAppointment);
   $('#users input[type="checkbox"]').on('click', clickChangeAdmin);
-  $('form#todo').on('submit', submitTodo);
   $('#generals').on('click', 'input[type="checkbox"]', clickChangeIsComplete);
 }
 
@@ -66,24 +64,6 @@ function clickChangeAdmin(){
   });
 }
 
-// function submitAppointment(e)
-// {
-//   debugger;
-//   var url = $(this).attr('action');
-//   var data = $(this).serialize();
-//   sendAjaxRequest(url, data, 'post', null, e, function(data){
-//     htmlAddAppointment(data);
-//   });
-// }
-
-function submitTodo(e){
-  var url = $(this).attr('action');
-  var data = $(this).serialize();
-  sendAjaxRequest(url, data, 'post', null, e, function(data){
-    htmlAddTodo(data);
-  });
-}
-
 function clickChangeIsComplete(){
   var id = $(this).parent().data('id');
   var url = '/general/' + id;
@@ -129,24 +109,11 @@ function htmlLogout(data){
   window.location.href = '/';
 }
 
-function htmlAddTodo(todo){
-  var tr = '<tr data-id="' + todo._id + '"><td><input type="checkbox"></td><td>' + todo.title + '</td><td>' + todo.category + '</td><td>' + todo.dueDate + '</td></tr>';
-  $('table#todos').append(tr);
-}
-
 function htmlChecked(box)
 {
-debugger;
-  // var $box = id.parent().parent();
   var $box = box;
   $box.addClass('finish');
 }
-
-// function htmlAddAppointment(appointment)
-// {
-//   var div = '<div data-id="' + appointment._id + '"><p class="title">' + appointment.title + '</p><p class="date">' + appointment.date + '</p><p class="content">' + appointment.content + '</p></div>';
-//   $('#appointments').append(div);
-// }
 
 // ------------------------------------------------------------------------- //
 // ------------------------------------------------------------------------- //
